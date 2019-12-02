6bfcaad9b8d8eaeee3c079828c3505818549f692
jfri3d <josh_friedman@outlook.com>
2019-12-02 18:38:04 +0100

    - generalized bot for different appointments + quantity - improved scheduler - added logging - improved README

--- main.py
+++ main.py
@@ -1,39 +1,43 @@
+import logging
+
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+
 from scraper import Scraper
 from telegram_bot import TelegramBot
-import schedule
-import time
 
-MINUTES = 10
+logging.basicConfig(format='%(asctime)s:%(levelname)s:%(message)s', level=logging.DEBUG)
+scheduler = BlockingScheduler()
+
+INTERVAL = 5  # in minutes
 scraper = Scraper()
 bot = TelegramBot()
 
 
-def get_message(appointments):
-    print(appointments)
+def get_message(appointments, send_message=False):
+    logging.debug(appointments)
     free_appointments = []
-    message = ""
-    send_message = False
     for date in appointments:
         value = appointments[date]
         if len(value) > 0:
             free_appointments.append(date)
-            print(date, value)
+            logging.info(date, value)
 
     if len(free_appointments) == 0:
-        message = f"Unfortunately I couldn't find any free appointment :( but I will keep you updated in {MINUTES} mins."
-        send_message = False
-        print(message)
+        message = f"Unfortunately I couldn't find any free appointment :( but I will keep you updated in {INTERVAL} mins."
+        logging.debug(message)
 
     else:
         url = "https://www46.muenchen.de/termin/index.php"
         message = f"I found these: {free_appointments}. Get your appointment here: {url}"
         send_message = True
-        print(message)
+        logging.debug(message)
 
     return message, send_message
 
 
 def get_appointments():
+    logging.info('Scraping appointments...')
     counter = 0
     while counter < 5:
         try:
@@ -48,17 +52,16 @@ def get_appointments():
         counter += 1
 
 
-def job():
-    print()
-    print('Cron job running...')
+@scheduler.scheduled_job(CronTrigger(minute=f"*/{INTERVAL}", hour='*', day='*', month='*', day_of_week='*'))
+def check_appointment():
+    logging.info('Starting scheduled job...')
     appointments = get_appointments()
     message, send_message = get_message(appointments)
     if send_message:
         bot.send_message(message)
 
 
-job()
-schedule.every(MINUTES).minutes.do(job)
-while 1:
-    schedule.run_pending()
-    time.sleep(1)
+if __name__ == "__main__":
+    logging.info('Initializing...')
+    scheduler.add_job(check_appointment)
+    scheduler.start()
